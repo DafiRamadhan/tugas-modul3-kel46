b05b93d0f4ddb4983f2b2d228677a86d3bbb4362
elvitro17 <89502020+elvitro17@users.noreply.github.com>
2021-09-18 11:53:45 +0700

    Update Menu.js

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,23 @@
 import { Component } from "react";
 import React from "react";
 import "./Menu.css";
+import Game from "./Game";
 import Makanan from "./Makanan";
 import Gambar1 from "../assets/images/image1.jpg";
 import Gambar2 from "../assets/images/image2.jpg";
 
 class Menu extends Component {
   state = {
+    game: false,
     makanan: false,
+  };
+  
+viewKomponen1 = () => {
+    this.setState(() => {
+      return {
+        game: !this.state.game,
+      };
+    });
   };
 
   viewKomponen2 = () => {
@@ -25,6 +35,15 @@ class Menu extends Component {
           Selamat datang, silakan pilih Menu dibawah ini!
         </h1>
         <div className="body">
+         <div className="component1">
+            <button onClick={this.viewKomponen1}>
+              {this.state.game ? "Tutup" : "Munculkan"} Daftar Game
+            </button>
+            {this.state.game && (
+              <Game image={Gambar1} image2={Gambar2} />
+            )}
+          </div>
+
           <div className="component2">
             <button onClick={this.viewKomponen2}>
               {this.state.makanan ? "Tutup" : "Munculkan"} Daftar Makanan
